b959f451035bd5e788444a7ff3b5a6a0de56efe6
Patryk Ostasz <patryk.ostasz@gmail.com>
2020-12-06 22:58:25 +0100

    day5 part2

--- day1/day5/main.cpp
+++ day1/day5/main.cpp
@@ -1,52 +1,93 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <sstream>
-#include <algorithm>
 #include <stack>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <unordered_set>
+
+#include "../Timer.h"
 
 using namespace std;
 
-
-unsigned findMatch(unsigned lb, unsigned ub, const string& patterns, const string& data, stack<unsigned>& unitIndices) {
-    static unsigned levelOfSearch = 0;
-    char searchPair[] = {data[lb], data[ub], '\0'};
-    string searchString(searchPair);
-    //cout << "[" << lb << ":" << ub << "] " << searchString << "\n";
-    if ((patterns.find(searchString) != string::npos)) {
-        if (levelOfSearch == 0 or ub == data.size()-1) return ub +1;
-        lb = unitIndices.top();
-        unitIndices.pop();
-        levelOfSearch--;
-        return findMatch(lb, ub+1, patterns, data, unitIndices);
-
-    }
-    levelOfSearch++;
-    unitIndices.push(lb);
-    return ub;
+void loadData(const string& filename, string& input) {
+    ifstream file(filename);
+    file >> input;
+    file.close();
 }
 
-int main()
-{
-    string inputData("");
-    string patterns;
+void createPatterns(string& patterns) {
     stringstream ss;
-
     unsigned asciiLetterOffset = 32;
     for (char i = 'A'; i <= 'Z'; ++i){
         ss << i << char(i+asciiLetterOffset) << " " << char(i+asciiLetterOffset) << i<<" ";
     }
     patterns = ss.str();
-    ifstream file("input.txt");
-    file >> inputData;
-    file.close();
-    stack<unsigned> polymerUnitsIndices;
-    polymerUnitsIndices.push(0);
-    unsigned indexStart = 0;
-    while(indexStart < inputData.size()-1) {
-        unsigned iBegin = indexStart;
-        indexStart = findMatch(iBegin, iBegin+1, patterns, inputData, polymerUnitsIndices);
+}
+
+
+unsigned findMatch(unsigned lb, unsigned ub, const string& patterns, const string_view& data, stack<unsigned>& unitIndices) {
+    char searchPair[] = {data[lb], data[ub], '\0'};
+    string searchString(searchPair);
+    if ((patterns.find(searchString) != string::npos)) {
+        if (ub == data.size()-1 or unitIndices.empty()) return ub + 1;
+        lb = unitIndices.top();
+        unitIndices.pop();
+        return findMatch(lb, ub + 1, patterns, data, unitIndices);
     }
-    cout << polymerUnitsIndices.size() << endl;
+    unitIndices.push(lb);
+    return ub;
+}
+
+unsigned getSolutionPart1(const string_view& data, const string& patterns) {
+    //Timer t;
+    stack<unsigned> polymerUnitsIndices;
+    unsigned indexStart = 0;
+    while(indexStart < data.size()-1) {
+        unsigned iBegin = indexStart;
+        indexStart = findMatch(iBegin, iBegin+1, patterns, data, polymerUnitsIndices);
+    }
+    if (polymerUnitsIndices.size() % 2 != 0) return polymerUnitsIndices.size() + 1;
+    return polymerUnitsIndices.size();
+}
+
+unsigned getSolutionPart2(const string& data, const string& pattern) {
+    vector<unsigned> polymerSizes;
+    polymerSizes.reserve(26);
+
+    string inputDataCopy;
+    inputDataCopy.reserve(data.size());
+    for (auto i = 0; i < pattern.size()-1; i+=6) {
+        char bigLetter = pattern[i], smallLetter = pattern[i+1];
+        inputDataCopy = data;
+
+        auto endIter = remove_if(begin(inputDataCopy), end(inputDataCopy),[bigLetter, smallLetter](auto val){return val == bigLetter or val == smallLetter;});
+
+        string_view reducedPolymer(inputDataCopy);
+        unsigned endPos = endIter - begin(inputDataCopy);
+        polymerSizes.emplace_back(getSolutionPart1(reducedPolymer.substr(0, endPos), pattern));
+    }
+    return *min_element(begin(polymerSizes), end(polymerSizes));
+}
+
+
+int main() {
+    string inputData, patterns;
+    createPatterns(patterns);
+    loadData("input.txt", inputData);
+
+    { // SOLUTION PART1
+        Timer t;
+        cout << getSolutionPart1(inputData, patterns) << endl;
+    }
+
+    { // SOLUTION PART2
+        Timer t;
+        cout << getSolutionPart2(inputData, patterns) << endl;
+    }
+
+
     return 0;
 }
